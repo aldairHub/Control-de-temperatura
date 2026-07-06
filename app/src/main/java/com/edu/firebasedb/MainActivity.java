8b290a0148f147d520e415ade63b167405f418fd
Freddy Farinangog <ffarinangog2@uteq.edu.ec>
2026-07-06 16:30:41 -0500

    first commit

---
+++ app/src/main/java/com/edu/firebasedb/MainActivity.java
@@ -0,0 +1,73 @@
+package com.edu.firebasedb;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database;
+    DatabaseReference HumedadRef, presionRef, VelocidadRef, TemperaturaRef;
+    EditText txt_temperaruta_edit, txt_humedad_edit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        database = FirebaseDatabase.getInstance();
+
+        HumedadRef = database.getReference("sensores/humedad");
+        presionRef = database.getReference("sensores/presion");
+        VelocidadRef = database.getReference("sensores/velocidad");
+        TemperaturaRef = database.getReference("sensores/temperatura");
+
+        TextView txtTemp = findViewById(R.id.valor_Temperatura);
+        TextView txtHum = findViewById(R.id.valor_Humedad);
+        TextView txtPres = findViewById(R.id.valor_Presion);
+        TextView txtVel = findViewById(R.id.valor_Velocidad);
+
+        txt_temperaruta_edit = findViewById(R.id.setvalor_Temperatura);
+        txt_humedad_edit = findViewById(R.id.setvalor_Humedad);
+
+        TemperaturaRef.addValueEventListener(setListener(txtTemp, "°C"));
+        HumedadRef.addValueEventListener(setListener(txtHum, "%"));
+        presionRef.addValueEventListener(setListener(txtPres, "hPa"));
+        VelocidadRef.addValueEventListener(setListener(txtVel, "km/h"));
+    }
+
+    public ValueEventListener setListener(TextView txt, String UnidadMedida) {
+        return (new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists() && snapshot.getValue() != null) {
+                    txt.setText(snapshot.getValue().toString() + " " + UnidadMedida);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                txt.setText("");
+            }
+        });
+    }
+
+    public void clickBotonTemperatura(View view) {
+        TemperaturaRef.setValue(txt_temperaruta_edit.getText().toString());
+    }
+
+    public void clickBotonHumedad(View view) {
+        TemperaturaRef.setValue(txt_humedad_edit.getText().toString());
+    }
+
+}
